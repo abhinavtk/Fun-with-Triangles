722e74dbfdedbd10ea678df28c216f1a188e2c24
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-12 13:43:17 +0530

    Update- triangle-quiz

--- triangle-quiz.js
+++ triangle-quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quiz-form");
 const submitBtn = document.querySelector("#submit-btn");
 const output = document.querySelector("#output");
 
-const correctAnswers =  ["90°", "equilateral", "Pythagorus"];
+const correctAnswers =  ["90°", "equilateral", "Pythagorus","Obtuse","70°"];
 
 function calculateScore() {
     let score = 0;
