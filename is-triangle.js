becc4970447693235a09e379a2dfe6f85dbbfa96
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-24 01:24:30 +0530

    fix - added alert when input is negative

--- is-triangle.js
+++ is-triangle.js
@@ -5,14 +5,23 @@ const output = document.querySelector("#output");
 function sumOfAngles(angle1, angle2, angle3){
     return angle1 + angle2 + angle3;
 }
-
-function isTriangle(){
-    const angleSum = sumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
-    if (angleSum === 180){
-        output.innerText = "The angles form a triangle.";
+function validateInput(angle1, angle2, angle3){
+    if(angle1<=0 || angle2<=0 || angle3<=0){
+        alert("Please enter valid input");
+        return false;
     }
-    else{
-        output.innerText = "The angles do not form a triangle.";
+    return true;
+}
+function isTriangle(){
+    var validate = validateInput(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if (validate){
+        const angleSum = sumOfAngles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+        if (angleSum === 180){
+            output.innerText = "The angles form a triangle.";
+        }
+        else{
+            output.innerText = "The angles do not form a triangle.";
+        }
     }
 }
 
