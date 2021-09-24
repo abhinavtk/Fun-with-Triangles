82cbadebf6cc473cc5b398275bf85fd72ca22955
Abhinav T K <63438668+abhinavtk@users.noreply.github.com>
2021-09-24 11:51:55 +0530

    fix - added alert when input less than 0

--- find-area.js
+++ find-area.js
@@ -2,9 +2,19 @@ const sideInputs = document.querySelectorAll(".side-input");
 const findAreaBtn = document.querySelector("#find-area");
 const output = document.querySelector("#output");
 
+function validateInput(side1, side2){
+    if(side1<=0 || side2<=0){
+        alert("Please enter values greater than 0");
+        return false;
+    }
+    return true;
+}
 function findArea(){
-    const area = (0.5*sideInputs[0].value*sideInputs[1].value).toFixed(2);
-    output.innerText = "The area is "+area+" units squared.";
+    var validate = validateInput(sideInputs[0].value, sideInputs[1].value)
+    if (validate){
+        const area = (0.5*sideInputs[0].value*sideInputs[1].value).toFixed(2);
+        output.innerText = "The area is "+area+" units squared.";
+    }
 }
 
 findAreaBtn.addEventListener('click', findArea);
--- find-hypotenuse.js
+++ find-hypotenuse.js
@@ -1,16 +1,24 @@
-const angles = document.querySelectorAll(".side-input");
+const sides = document.querySelectorAll(".side-input");
 const findHypotenuseBtn = document.querySelector("#find-hypotenuse");
 const output = document.querySelector('#output');
 
 function sumOfSquares(a, b){
     return a**2 + b**2;
 }
-
+function validateInput(side1, side2){
+    if(side1<=0 || side2<=0){
+        alert("Please enter values greater than 0");
+        return false;
+    }
+    return true;
+}
 function findHypotenuse(){
-    const sumofSq = sumOfSquares(angles[0].value, angles[1].value);
-    const hypotenuse = Math.sqrt(sumofSq).toFixed(2);
-    output.innerText = "Hypotenuse length is "+ hypotenuse+ " units.";
+    var validate = validateInput(sides[0].value, sides[1].value);
+    if (validate){
+        const sumofSq = sumOfSquares(sides[0].value, sides[1].value);
+        const hypotenuse = Math.sqrt(sumofSq).toFixed(2);
+        output.innerText = "Hypotenuse length is "+ hypotenuse+ " units.";
+    } 
 }
 
-findHypotenuseBtn.
-addEventListener('click', findHypotenuse);
+findHypotenuseBtn.addEventListener('click', findHypotenuse);
